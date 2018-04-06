f566305dd91f99a8789ee7cded8713b7405b2679
geocfu <geocfuone@gmail.com>
2018-04-07 01:30:48 +0300

    Added functions for password hash and account availability.

--- register.php
+++ register.php
@@ -1,10 +1,12 @@
 <?php
-//Display the errors
+//Display errors if any
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
+//Check if client connection is of type POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     //Check if POST data exists as variables and are not empty
     if ((isset($_POST["email"]) && !empty($_POST["email"])) &&
         (isset($_POST["password"]) && !empty($_POST["password"])) &&
@@ -13,7 +15,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         //Password must include at least one uppercase and one lowercase characters, one number and one special charachter
         $passwordPattern = "/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,32}$/";
 
-        //mail
+        /*
+        Check email and if is valid, save it and raise the respective flag.
+        If email is invalid dont save it and raise respective flag.
+        */
         if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
             $email = checkInput($_POST["email"]);
             $emailState = "VALID";
@@ -21,7 +26,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         else {
             $emailState = "INVALID";
         }
-        //password
+
+        /*
+        Check password and if is valid, save it and raise the respective flag.
+        If password is invalid dont save it and raise respective flag.
+        */
         if (preg_match($passwordPattern, $_POST["password"])) {
             $password = checkInput($_POST["password"]);
             $passwordState = "VALID";
@@ -29,7 +38,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         else {
             $passwordState = "INVALID";
         }
-        //repeat_password
+
+        /*
+        Check repeat_password and if is valid, save it and raise the respective flag.
+        If repeat_password is invalid dont save it and raise respective flag.
+        */
         if (preg_match($passwordPattern, $_POST["repeat_password"])) {
             $repeat_password = checkInput($_POST["repeat_password"]);
             $repeatPasswordState = "VALID";
@@ -38,25 +51,54 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $repeatPasswordState = "INVALID";
         }
 
-        //Chech if password and repeat_password are valid and match
-        if (strcmp(strcmp($passwordState, $repeatPasswordState), "VALID") == 0) {
-            $credentials = array("email" => $email,
-                                "password" => $password);
+        //Chech if email, password and repeat_password are initialized.
+        if (isset($email) && isset($password) && isset($repeat_password)) {
 
-            if (!file_exists($_SERVER['DOCUMENT_ROOT'] . "/wols/userdata/")) {
-                mkdir($_SERVER['DOCUMENT_ROOT'] . "/wols/userdata/");
+            //Chech if password and repeat_password match.
+            if (strcmp($password, $repeat_password) == 0) {
+                /*
+                Check if the required directory exists.
+                If does not exist, create it.
+                */
+                if (!file_exists($_SERVER['DOCUMENT_ROOT'] . "/wols/userdata/")) {
+                    mkdir($_SERVER['DOCUMENT_ROOT'] . "/wols/userdata/");
+                }
+                //Check if account exists. If does not, create it.
+                if (checkAccountAvailability ($email)) {
+                    //Save email and hashed password.
+                    $credentials = array("email" => $email,
+                                        "passwordHash" => hashPassword($password));
+                    //Export the credentials as json.
+                    file_put_contents($_SERVER['DOCUMENT_ROOT'] .
+                        "/wols/userdata/$email.json", json_encode($credentials, JSON_PRETTY_PRINT));
+                    //Raise the appropriate flags.
+                    $passwordMatchState = true;
+                    $serverState = "SUCCESS";
+                }
+                //Account exixts, exit with appropriate message.
+                else {
+                    die("ACCOUNT_DOES_EXIST");
+                }
+
             }
-            file_put_contents($_SERVER['DOCUMENT_ROOT'] . "/wols/userdata/$email.json", json_encode($credentials, JSON_PRETTY_PRINT));
-
-            $passwordMatchState = true;
-
-            $serverState = "SUCCESS";
+            /*Password and repeat_password dont match.
+            Raise the appropriate flags.
+            */
+            else {
+                $passwordMatchState = false;
+                $serverState = "FAILURE";
+            }
         }
+        /*
+        Email or password or repeat_password does not comply with the rules; Dont accept.
+        Raise the appropriate flags.
+        */
         else {
             $passwordMatchState = false;
             $serverState = "FAILURE";
         }
 
+        //Respond to the clients request with json output (testing perpuses).
         $serverResponse = array("Email" => $emailState,
                                 "Password" => $passwordState,
                                 "Repeat_Password" => $repeatPasswordState,
@@ -66,24 +108,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $jsonServerResponse = json_encode($serverResponse);
         echo $jsonServerResponse;
     }
+    //Handle missing post variables.
     else {
         if (!isset($_POST["email"]) || !isset($_POST["password"]) || !isset($_POST["repeat_password"])) {
             die("FORM_DATA_MISSING");
         }
-        elseif (empty($_POST["email"]) || empty($_POST["password"]) || empty($_POST["repeat_password"])) {
+        else {
             die("FORM_DATA_EMPTY");
         }
     }
 }
+//Handle error if connection is not of type POST.
 else {
-    echo "POST_REQUIRED";
-    die();
+    die("POST_REQUIRED");
 }
 
+//Comply the input with only accepted characters.
 function checkInput ($input) {
     $input = trim($input);
     $input = stripslashes($input);
     $input = htmlspecialchars($input);
     return $input;
+}
+//Generate password hash.
+function hashPassword ($password) {
+    $options = [
+        'cost' => 11,
+    ];
+    $passwordHash = password_hash($password, PASSWORD_BCRYPT, $options);
+    return $passwordHash;
+}
+//Check if account exists or not.
+function checkAccountAvailability ($email) {
+    if (file_exists($_SERVER['DOCUMENT_ROOT'] . "/wols/userdata/". $email .".json")) {
+        return false;
+    }
+    return true;
 }
 ?>
