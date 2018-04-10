9b38f5a8a92d1be2e1eaceb4442f6120843422dd
geocfu <geocfuone@gmail.com>
2018-04-10 19:55:44 +0300

    Swap email varibale to username

--- registration.php
+++ registration.php
@@ -8,18 +8,20 @@ ini_set('display_startup_errors', 1);
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     //Check if POST data exists as variables and are not empty
-    if ((isset($_POST["email"]) && !empty($_POST["email"])) &&
+    if ((isset($_POST["username"]) && !empty($_POST["username"])) &&
         (isset($_POST["password"]) && !empty($_POST["password"]))) {
 
+        //Username must not include any special characters and must be at least 5 characters long.
+        $usernamePattern = "/^([a-zA-Z0-9]){5,20}$/";
         //Password must include at least one uppercase and one lowercase characters, one number and one special character.
         $passwordPattern = "/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{9,32}$/";
 
-        //Check email and if is valid, save it. If email is invalid abort the connection.
-        if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-            $email = $_POST["email"];
+        //Check USERNAME and if is valid, save it. If email is invalid abort the connection.
+        if (preg_match($usernamePattern, $_POST["username"])) {
+            $username = checkInput($_POST["username"]);
         }
         else {
-            die("INVALID_EMAIL");
+            die("INVALID_USERNAME");
         }
 
         //Check password and if is valid, save it. If password is invalid abort the connection.
@@ -31,7 +33,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
 
         //Chech if the initialization of email, password and repeat_password comply.
-        if (isset($email) && isset($password)) {
+        if (isset($username) && isset($password)) {
 
             //Check if the required directory exists. If does not exist, create it.
             if (!file_exists($_SERVER['DOCUMENT_ROOT'] . "/wols/userdata/")) {
@@ -39,20 +41,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             }
 
             //Check if account exists. If does not, create it.
-            if (!accountExists($email)) {
+            if (!accountExists($username)) {
 
                 date_default_timezone_set('Europe/Athens');
 
                 $passwordHash = hashInput($password);
-                $apiKey = hash_hmac("sha512/256", random_bytes(512), $passwordHash);
-                $credentials = array("email" => $email,
+                $apiKey = generateUniqueApiKey();
+                $apiKeyHash = hashInput($apiKey);
+
+                $credentials = array("username" => $username,
                                     "passwordHash" => $passwordHash,
-                                    "apiKey" => $apiKey,
+                                    "apiKeyHash" => $apiKeyHash,
                                     "dateCreated" => date_format(date_create(), 'Y-m-d H:i:s'));
 
                 //Export the credentials as json.
                 file_put_contents($_SERVER['DOCUMENT_ROOT'] .
-                    "/wols/userdata/$email.json", json_encode($credentials, JSON_PRETTY_PRINT));
+                    "/wols/userdata/$username.json", json_encode($credentials, JSON_PRETTY_PRINT));
 
                 $serverResponse = array("API_KEY" => $apiKey);
                 echo json_encode($serverResponse);
@@ -64,7 +68,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     //Handle missing post variables.
     else {
-        if (!isset($_POST["email"]) || !isset($_POST["password"]) || !isset($_POST["repeat_password"])) {
+        if (!isset($_POST["username"]) || !isset($_POST["password"])) {
             die("FORM_DATA_MISSING");
         }
         else {
@@ -92,8 +96,12 @@ function hashInput ($input) {
     return $inputHash;
 }
 
-function accountExists ($email) {
-    if (file_exists($_SERVER['DOCUMENT_ROOT'] . "/wols/userdata/$email.json")) {
+function generateUniqueApiKey () {
+    return hash_hmac("sha512/256", bin2hex(random_bytes(1024)), bin2hex(random_bytes(1024)));
+}
+
+function accountExists ($username) {
+    if (file_exists($_SERVER['DOCUMENT_ROOT'] . "/wols/userdata/$username.json")) {
         return true;
     }
     return false;
