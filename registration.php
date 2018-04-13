97210e9e19e23fede22c7ff4fbd977b54590659b
geocfu <geocfuone@gmail.com>
2018-04-13 03:42:10 +0300

    With user's registration, user also sends name & mac of computer/s

--- registration.php
+++ registration.php
@@ -9,12 +9,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     //Check if POST data exists as variables and are not empty
     if ((isset($_POST["username"]) && !empty($_POST["username"])) &&
-        (isset($_POST["password"]) && !empty($_POST["password"]))) {
+        (isset($_POST["password"]) && !empty($_POST["password"])) &&
+        (isset($_POST["local_pc_names"]) && !empty($_POST["local_pc_names"]))) {
 
         //Username must not include any special characters and must be at least 5 characters long.
         $usernamePattern = "/^([a-zA-Z0-9]){5,20}$/";
+
         //Password must include at least one uppercase and one lowercase characters, one number and one special character.
         $passwordPattern = "/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{9,32}$/";
+
+        //Mac address must have 5 colons in between the characters.
+        $macAddressPattern = "/^([a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2}$/";
 
         //Check USERNAME and if is valid, save it. If email is invalid abort the connection.
         if (preg_match($usernamePattern, $_POST["username"])) {
@@ -32,8 +37,70 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             die("INVALID_PASSWORD");
         }
 
+
+        if (substr_count($_POST["local_pc_names"], ",")) {
+
+            $localComputersArray = explode(",", $_POST["local_pc_names"]);
+
+            foreach ($localComputersArray as $localComputer) {
+
+                if (substr_count($localComputer , "-")) {
+                    $macAndNameOfLocalComputer =  explode("-", $localComputer);
+
+                    if (preg_match($macAddressPattern, $macAndNameOfLocalComputer[0])) {
+                        $computerMac = checkInput($macAndNameOfLocalComputer[0]);
+                    }
+                    else {
+                        die("INVALID_MAC_ADDRESS");
+                    }
+
+                    if (preg_match($usernamePattern, $macAndNameOfLocalComputer[1])) {
+                        $computerName = checkInput($macAndNameOfLocalComputer[1]);
+                    }
+                    else {
+                        die("INVALID_COMPUTER_NAME");
+                    }
+
+                    $localComputersMacAndName[] = array("computerName" => $computerName,
+                                                    "computerMac" => $computerMac);
+                }
+                //invalid computer name
+                else {
+                    die("INVALID_LOCAL_PC_ΝΑΜΕ_SYNTAX");
+                }
+            }
+        }
+        //only one pc was submited
+        else {
+            if (substr_count($_POST["local_pc_names"] , "-")) {
+                $macAndNameOfLocalComputer =  explode("-", $_POST["local_pc_names"]);
+
+                if (preg_match($macAddressPattern, $macAndNameOfLocalComputer[0])) {
+                    $computerMac = checkInput($macAndNameOfLocalComputer[0]);
+                }
+                else {
+                    die("INVALID_MAC_ADDRESS");
+                }
+
+                if (preg_match($usernamePattern, $macAndNameOfLocalComputer[1])) {
+                    $computerName = checkInput($macAndNameOfLocalComputer[1]);
+                }
+                else {
+                    die("INVALID_COMPUTER_NAME");
+                }
+
+                $localComputersMacAndName[] = array("computerName" => $computerName,
+                                                "computerMac" => $computerMac);
+            }
+            //invalid computer name
+            else {
+                die("INVALID_LOCAL_PC_ΝΑΜΕ_SYNTAX");
+            }
+        }
+
+
         //Chech if the initialization of email, password and repeat_password comply.
-        if (isset($username) && isset($password)) {
+        if (isset($username) && isset($password) && isset($computerMac) && isset($computerName)) {
 
             //Check if the required directory exists. If does not exist, create it.
             if (!file_exists($_SERVER['DOCUMENT_ROOT'] . "/wols/userdata/")) {
@@ -52,6 +119,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $credentials = array("username" => $username,
                                     "passwordHash" => $passwordHash,
                                     "apiKeyHash" => $apiKeyHash,
+                                    "compuetersInLocalNetwork" => $localComputersMacAndName,
                                     "dateCreated" => date_format(date_create(), 'Y-m-d H:i:s'));
 
                 //Export the credentials as json.
@@ -68,7 +136,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     //Handle missing post variables.
     else {
-        if (!isset($_POST["username"]) || !isset($_POST["password"])) {
+        if (!isset($_POST["username"]) || !isset($_POST["password"]) || !isset($_POST["local_pc_names"])) {
             die("FORM_DATA_MISSING");
         }
         else {
